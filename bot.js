a548a32b4ebf62a333386314e18f3e7f74ff528f
recredit <46069475+recredit@users.noreply.github.com>
2019-01-23 14:46:14 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("537306746906214414")
+setInterval(function() {
+channel.send(`tmeeeeeeeeeeeeeeeeeeees`);
+}, 30)
+})
+
+client.login(process.env."NTI2MDM1ODk0MDcxOTg0MTMy.DyndVw.P3Y7XVnToQy2DNRKpviXAYMkNe4");
